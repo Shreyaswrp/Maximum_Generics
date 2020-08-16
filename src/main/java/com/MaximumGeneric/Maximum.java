0195842e11b9ccc26406b0b5fd015db5c36a3089
Shreya Swaroop <shreyaswrp@gmail.com>
2020-08-16 20:01:32 +0530

    Added:to extend number of parameters

--- src/main/java/com/MaximumGeneric/Maximum.java
+++ src/main/java/com/MaximumGeneric/Maximum.java
@@ -1,22 +1,27 @@
 package com.MaximumGeneric;
 
-public class Maximum <E extends Comparable>{
-    E firstValue;
-    E secondValue;
-    E thirdValue;
+public class Maximum<E extends Comparable> {
+    E[] inputArray;
 
-    public Maximum(E firstValue, E secondValue, E thirdValue) {
-        this.firstValue = firstValue;
-        this.secondValue = secondValue;
-        this.thirdValue = thirdValue;
+    public Maximum(E[] inputArray) {
+        this.inputArray = inputArray;
     }
 
-    public  E getMax() {
-        if ( firstValue.compareTo(secondValue) > 0 && firstValue.compareTo(thirdValue) > 0 )
-            return firstValue;
-        else if ( secondValue.compareTo(firstValue) > 0 && secondValue.compareTo(thirdValue) > 0 )
-            return secondValue;
-        else
-            return thirdValue;
+    public E getMax(E[] inputArray) {
+        E max = inputArray[0];
+        for ( int i = 1; i < inputArray.length; i++ )
+            if ( inputArray[i].compareTo(max) > 0 )
+                max = inputArray[i];
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Integer[] intArray={1,2,3};
+        Float[] floatsArray={1.1f,2.2f,3.3f,4.4f};
+        String[] stringsArray={"apple","banana","orange","mango"};
+
+        Comparable maxInt = new Maximum(intArray).getMax(intArray);
+        Comparable maxFloat = new Maximum(floatsArray).getMax(floatsArray);
+        Comparable maxString = new Maximum(stringsArray).getMax(stringsArray);
     }
 }
